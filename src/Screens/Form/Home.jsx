6e5637e89f925f4dc4f7361787cbd010378d041e
Ammar Karim <azlankarim660@gmail.com>
2023-04-28 08:21:01 +0500

    Add files via upload

--- src/Screens/Form/Home.jsx
+++ src/Screens/Form/Home.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from '../../Assets/Images/logo.png'
 import { Link } from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function Home() {
+  const [user , setUser] = useState('')
+  const [admin , setAdmin] = useState('')
+  const getUserFromDataBase = () => {
+    const auth = getAuth();
+    onAuthStateChanged(auth,(user) => {
+
+      if (user) {
+        setUser(true)
+        user.email==='admin@gmail.com'?setAdmin(true):setAdmin(false);
+        
+      } else {
+        setUser(false)
+      }
+    });
+  }
+
+    useEffect(() => {
+      getUserFromDataBase();
+    }, [""])
+    
+
   return (
     <>
       <div className="getStarted_main_div">
@@ -12,7 +34,7 @@ function Home() {
           <p>ONLINE DISCOUNT STORE</p>
         </div>
         <div className="signup_button_div">
-          <button className='get_Started_button'> <Link to='/Login'className='link'  > GET STARTED</Link> </button>
+          <button className='get_Started_button'> {user?admin?<Link to='/admin/home'className='link'  > GET STARTED</Link>:<Link to='/user/home/page'className='link'  > GET STARTED</Link>:<Link to='/Login'className='link'  > GET STARTED</Link>} </button>
         </div>
       </div>
     </>  
